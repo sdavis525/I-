aab3d1555ae6981de845dcb6cc77431fb7d47712
Ed Kubiak <ek33450505@gmail.com>
2022-05-13 17:12:42 -0400

    push to pull

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,15 +1,8 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
-<<<<<<< HEAD
 
 
 //GET ALL POSTS  -  
-=======
-const withAuth = require('../../utils/auth');
-
-
-//GET ALL POSTS  -  /api/posts
->>>>>>> d29c83bdb79a0a3993c28d49cf87bfeaee370f03
 router.get('/', (req, res) => {
     Post.findAll({
         order: [['created_at', 'DESC']],
@@ -34,21 +27,12 @@ router.get('/', (req, res) => {
 })
 
 //CREATE POST
-<<<<<<< HEAD
 router.post('/', (req, res) => {
     Post.create({
         title: req.body.title,
         post_text: req.body.post_text,
         //change to req.session.user_id after session setup
         user_id: req.body.user_id
-=======
-router.post('/', withAuth, (req, res) => {
-    Post.create({
-        title: req.body.title,
-        post_text: req.body.post_text,
-        //find user id based off off session object
-        user_id: req.session.user_id
->>>>>>> d29c83bdb79a0a3993c28d49cf87bfeaee370f03
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -59,11 +43,7 @@ router.post('/', withAuth, (req, res) => {
 
 
 //EDIT POST
-<<<<<<< HEAD
 router.put('/:id', (req, res) => {
-=======
-router.put('/:id', withAuth, (req, res) => {
->>>>>>> d29c83bdb79a0a3993c28d49cf87bfeaee370f03
     Post.update(
         {
             title: req.body.title,
@@ -75,17 +55,7 @@ router.put('/:id', withAuth, (req, res) => {
             }
         }
     )
-<<<<<<< HEAD
         .then(dbPostData => res.json(dbPostData))
-=======
-    .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-    })
->>>>>>> d29c83bdb79a0a3993c28d49cf87bfeaee370f03
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -93,38 +63,19 @@ router.put('/:id', withAuth, (req, res) => {
 })
 
 //DELETE POST
-<<<<<<< HEAD
 router.delete('/:id', (req, res) => {
-=======
-router.delete('/:id', withAuth, (req, res) => {
->>>>>>> d29c83bdb79a0a3993c28d49cf87bfeaee370f03
     Post.destroy({
         where: {
             id: req.params.id
         }
     }
     )
-<<<<<<< HEAD
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         })
 })
-=======
-    .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
->>>>>>> d29c83bdb79a0a3993c28d49cf87bfeaee370f03
 
 
 module.exports = router;
