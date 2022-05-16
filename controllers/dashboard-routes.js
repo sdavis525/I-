9fc144862a63f822924caeb9bbaa31bb1f24cadb
Ed Kubiak <ek33450505@gmail.com>
2022-05-16 15:55:11 -0400

    dashboardroutes is causing error

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -13,7 +13,7 @@ router.get('/', withAuth, (req, res) => {
         },
         attributes: [
             'id', 
-            'post_url',
+            'post_text',
             'title',
             'created_at'
         ],
--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,10 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 
 const apiRoutes = require('./api');
-const dashboardRoutes = require('./dashboard-routes');
-const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
+// router.use('/dashboardRoutes');
 
 router.use((req, res) => {
   res.status(404).end();
