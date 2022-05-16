50efd4f9f338238dc7b18a0e10279db560c0a7b0
ancs214 <ancs214@gmail.com>
2022-05-16 10:50:36 -0400

    created dashboard GET route

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes');
+
 
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+
 
 router.use((req, res) => {
   res.status(404).end();
