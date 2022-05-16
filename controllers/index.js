baf024c0ba2346b3d1d70a0ec4ea6301ba4b72d8
Ed Kubiak <ek33450505@gmail.com>
2022-05-16 12:05:23 -0400

    added dashboard-routes and updated home-routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 
 router.use((req, res) => {
