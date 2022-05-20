9fddeac94261001c37f6c0e6d60d305c914661fc
ancs214 <ancs214@gmail.com>
2022-05-19 20:26:58 -0400

    small changes to make templates show our info

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -92,7 +92,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to edit-post.handlebars template (to be made)
-      res.render('edit-post', { post, loggedIn: true });
+      res.render('edit-post', { posts, loggedIn: true });
     })
     .catch(err => {
       console.log(err);
