dca0cc123aeb079b0a4caafae4772def66133561
Ed Kubiak <ek33450505@gmail.com>
2022-05-20 19:46:50 -0400

    reworked dashboardhandlebars

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,12 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
@@ -16,3 +15,4 @@ if (process.env.JAWSDB_URL) {
 }
 
 module.exports = sequelize;
+
