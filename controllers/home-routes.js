91f0af39f5f37bbcdc5678b7ec11d2286d042a34
ancs214 <ancs214@gmail.com>
2022-05-20 21:43:09 -0400

    added styling and function to dashboard page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('homepage', { posts });
+      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
     })
     .catch(err => {
       console.log(err);
@@ -38,7 +38,12 @@ router.get('/', (req, res) => {
     });
 });
 
+//LOGIN
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
   res.render('login');
 });
 
