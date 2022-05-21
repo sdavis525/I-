d57675eafdc72e487d70b1bb2624e2cbfa02365d
ancs214 <ancs214@gmail.com>
2022-05-21 00:42:45 -0400

    started making single-post handlebars page. found new error when creating user from login page.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,6 +40,49 @@ router.get('/', (req, res) => {
         });
 });
 
+
+//GET SINGLE POST FOR SINGLE-POST PAGE
+router.get('/posts/:id', (req, res) => {
+    console.log(req.session);
+    Post.findOne({
+        where: {
+            id:req.params.id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM heart WHERE post.id = heart.post_id)'), 'heart_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            //loop over and map each sequelize object into a serialized version of itself
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            //render object 'posts' to homepage.handlebars file
+            res.render('single-post', { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
 //LOGIN
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
