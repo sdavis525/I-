188d4d43931317c06c111de7fa13143f7b3386b6
ancs214 <ancs214@gmail.com>
2022-05-22 09:40:56 -0400

    comment functionality on singlepost page working

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,4 +1,4 @@
- function editFormHandler(event) {
+ async function editFormHandler(event) {
   event.preventDefault();
   
   const title = document.querySelector('input[name="post-title"]').value.trim();
@@ -10,22 +10,22 @@
 
 console.log('clicked!!' + title + post_text + id)
 
-  // const response = await fetch(`/api/posts/${id}`, {
-  //   method: 'PUT',
-  //   body: JSON.stringify({
-  //     title,
-  //     post_text
-  //   }),
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   }
-  // });
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      post_text
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
-  // if (response.ok) {
-  //   document.location.replace('/dashboard/');
-  // } else {
-  //   alert(response.statusText);
-  // }
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document.querySelector('.edit-post-btn').addEventListener('submit', editFormHandler);
+document.querySelector('.edit-btn').addEventListener('submit', editFormHandler);
